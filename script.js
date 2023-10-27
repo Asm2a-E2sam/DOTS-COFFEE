6fe59fe7def3950d7aac65a02f17b91009a8fc69
Asmaa E Soliman <94235540+Asm2a-E2sam@users.noreply.github.com>
2023-10-27 18:52:49 +0200

    edit register form

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ import * as cookie from "./cookie-lib.js";
 let date = new Date();
 date.setMonth(date.getMonth() + 1);
 
-let usernameRegx = /^[a-z]{3,10}[0-9]{0,4}$/;
-let emailRegx = /^[a-z]{3,10}[0-9]{0,4}@(gmail|yahoo)(.com)$/;
-let passwordRegx = /^[a-zA-Z]{3,5}[0-9]{3}$/;
+let usernameRegx = /^[a-zA-Z0-9]{5,20}$/;
+let emailRegx = /^[a-z0-9]{5,30}@(gmail|yahoo)(.com)$/;
+let passwordRegx = /^[a-zA-Z0-9]{8,20}$/;
 
 document.getElementById("register").addEventListener("click",function(){
     let username = document.getElementById("username").value;
@@ -21,14 +21,14 @@ document.getElementById("register").addEventListener("click",function(){
           location.href = "login.html"
         } else {
           alert(
-            "Please enter a valid Password contain characters&numbers like Xyz2023"
+            "Please enter a valid Password contain only characters&numbers and min length 8"
           );
         }
       } else {
-        alert("invalid Email example(sample123@gmail.com)");
+        alert("invalid Email it must be (gmail or yahoo) example(sample123@gmail.com)");
       }
     } else {
-      alert("Invalid Username example(xyz123)");
+      alert("Invalid Username contain only characters&numbers and min length 5");
     }
 })
 
